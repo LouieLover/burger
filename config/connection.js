42a964faa2a0e70d4736634c085e223bf8759369
Larry X <papadocarock@gmail.com>
2020-10-06 15:36:13 -0400

    setting up files

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "127.0.0.1",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "1234",
+    database: "burgers"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+
+
+});
+
+module.exports = connection;

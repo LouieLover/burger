1c4236f9725cf2e7b9031460fdaad8295861111f
Larry X <papadocarock@gmail.com>
2020-10-09 14:58:45 -0400

    updated code

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,23 @@
 const mysql = require("mysql");
+var connection
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "127.0.0.1",
 
-var connection = mysql.createConnection({
-    host: "127.0.0.1",
+        // Your port; if not 3306
+        port: 3300,
 
-    // Your port; if not 3306
-    port: 3300,
+        // Your username
+        user: "root",
 
-    // Your username
-    user: "root",
+        // Your password
+        password: "1234",
+        database: "burgers"
+    });
+}
 
-    // Your password
-    password: "1234",
-    database: "burgers"
-});
 
 connection.connect(function(err) {
     if (err) throw err;
--- models/burgers.js
+++ models/burgers.js
@@ -25,40 +25,5 @@ var burgers = {
 };
 
 
-// const burgers = selectAll + insertOne + updateOne + deleteOne;
-
-// // burgers();
-
-// function selectAll() {
-//     connection.query("SELECT * FROM burgers", function(err, res) {
-//         if (err) throw err;
-//         console.log(res);
-//     });
-// }
-
-
-// function insertOne() {
-//     connection.query("INSERT INTO burgers SET ?", function(err, res) {
-//         if (err) throw err;
-//         console.log(res);
-//     });
-// }
-
-// function updateOne() {
-//     connection.query("UPDATE burgers SET ? WHERE ? ", function(err, res) {
-//         if (err) throw err;
-//         console.log(res);
-//     });
-// }
-
-
-// function deleteOne() {
-//     connection.query("DELETE FROM burgers WHERE ? ", function(err, res) {
-//         if (err) throw err;
-//         console.log(res);
-//     });
-// }
-
-
 
 module.exports = burgers;

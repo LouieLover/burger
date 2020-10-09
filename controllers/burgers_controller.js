d702ea52cbd1104ae355223a1432f46d9f6271b6
Larry X <papadocarock@gmail.com>
2020-10-09 18:13:47 -0400

    update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,7 +11,7 @@ router.get("/", function(req, res) {
             burgers: data
         };
         console.log(hdbrsOBJ);
-        res.render("index", hdbrsOBJ, req);
+        res.render("index", hdbrsOBJ);
     });
 
     router.post("/api/burgers", function(req, res) {
